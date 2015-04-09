ded94c861e38a0a4455937054171607bb2aa9fca
Tony Crisci <tony@dubstepdish.com>
2015-04-09 18:56:54 -0400

    Add fpm dist script

---
+++ fpm-deb.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+rm -rf /tmp/playerctl-fpm
+./configure --prefix=/usr
+make
+VERSION=`./playerctl/playerctl -V | sed s/^v//`
+DESTDIR=/tmp/playerctl-fpm make install
+
+cd /tmp/playerctl-fpm
+
+fpm -s dir -t deb -n playerctl -v $VERSION \
+    -p playerctl-VERSION_ARCH.deb \
+    -d "libglib2.0-0" \
+    usr/include usr/lib usr/bin usr/share/gir-1.0
+
+cd -
+
+mv /tmp/playerctl-fpm/playerctl-${VERSION}_amd64.deb ./
+
+rm -r /tmp/playerctl-fpm
+
+echo -e "\nPACKAGE CONTENTS\n"
+
+dpkg -c ./playerctl-${VERSION}_amd64.deb
