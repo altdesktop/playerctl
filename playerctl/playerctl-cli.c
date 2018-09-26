aeaf573719741f5d300126ab7ca9471633c3fcd1
Tony Crisci <tony@dubstepdish.com>
2018-09-26 11:11:00 -0400

    remove copyright date ranges

--- playerctl/playerctl-cli.c
+++ playerctl/playerctl-cli.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with playerctl If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright © 2014 - 2016, Tony Crisci and contributors.
+ * Copyright © 2014, Tony Crisci and contributors.
  */
 
 #include <gio/gio.h>
