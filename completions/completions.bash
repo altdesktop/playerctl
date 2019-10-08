e1246e62129448762666b641a1efe15b1d2ec419
Mark Gross <mark.gross2001@gmail.com>
2019-10-08 13:29:02 -0700

    Add bash completions
    Create the /completions directory and add a completion script for bash

---
+++ completions/completions.bash
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+_playerctl_completions() {
+	local cur="${COMP_WORDS[$COMP_CWORD]}"
+	local prev="${COMP_WORDS[$COMP_CWORD - 1]}"
+	local root_words="
+		play
+		pause
+		play-pause
+		stop
+		next
+		previous
+		position
+		volume
+		status
+		metadata
+		open
+		loop
+		shuffle
+		-h --help
+		-p --player=
+		-a --all-players
+		-i --ignore-player=
+		-f --format
+		-F --follow
+		-l --list-all
+		-v --version"
+
+	case $prev in 
+		loop)
+			COMPREPLY=($(compgen -W "none track playlist" -- "$cur"))
+			return 0
+			;;
+		shuffle)
+			COMPREPLY=($(compgen -W "on off" -- "$cur"))
+			return 0
+			;;
+		-p|--player=|-i|--ignore-player=)
+			# TODO list supported players here?
+			COMPREPLY=()
+			return 0
+			;;
+		-f|--format)
+			COMPREPLY=()
+			return 0
+			;;
+		position|volume|metadata|open)
+			COMPREPLY=()
+			return 0
+			;;
+		*)
+			COMPREPLY=($(compgen -W "$root_words" -- "$cur"))
+			return 0
+			;;
+	esac
+}
+
+complete -F _playerctl_completions playerctl
