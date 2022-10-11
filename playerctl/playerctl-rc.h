cc8f39bb53b9fe534651f06b8340803afcc4ea85
Vegard Løkken <vegard@loekken.org>
2022-10-12 01:06:44 +0200

    Add support for playerctlrc file
    Instead of having to specify the same options each time, common options
    (e.g. --ignore-player) may be placed inside $HOME/.playerctlrc (or
    globally in /etc/playerctlrc). These options are parsed before cli
    arguments, making it possible for manual override.

---
+++ playerctl/playerctl-rc.h
@@ -0,0 +1,25 @@
+/*
+ * This file is part of playerctl.
+ *
+ * playerctl is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * playerctl is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with playerctl If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright © 2022, Tony Crisci and contributors
+ */
+
+#ifndef __PLAYERCTL_RC_H__
+#define __PLAYERCTL_RC_H__
+
+char **playerctl_rc_read_options();
+
+#endif /* __PLAYERCTL_RC_H__ */
